37278d841ff68d7862e47f458247888623e64c65
Douglas Coble <>
2018-04-04 11:56:36 -0400

    fix users controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersController } from 'users/users.controller';
 import { UsersService } from 'users/users.service';
+import { UsersModule } from 'users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    UsersController,
-  ],
-  components: [UsersService],
+  imports: [UsersModule],
 })
 export class AppModule {}
