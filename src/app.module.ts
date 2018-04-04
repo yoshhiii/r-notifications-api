e4332662fbeed2785e4d660ee2cff3104e83ce2a
Douglas Coble <>
2018-04-04 14:30:59 -0400

    fix import paths on app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UsersModule } from 'users/users.module';
-import { AlertsModule } from 'alerts/alerts.module';
-import { DepartmentsModule } from 'departments/departments.module';
+import { UsersModule } from './users/users.module';
+import { AlertsModule } from './alerts/alerts.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
   imports: [
