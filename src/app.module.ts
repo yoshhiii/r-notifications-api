d7b23959ab662876f2b1182a15602ba136d5384c
Douglas Coble <>
2018-04-04 11:24:09 -0400

    import users controller on main app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersController } from 'users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    UsersController,
+  ],
   components: [],
 })
 export class AppModule {}
