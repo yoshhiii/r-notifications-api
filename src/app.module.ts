c7fd659fb42708ca1cb27a82a4ef71b89d55b7f6
Douglas Coble <>
2018-04-04 11:47:16 -0400

    add user service to app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersController } from 'users/users.controller';
+import { UsersService } from 'users/users.service';
 
 @Module({
   imports: [],
@@ -8,6 +9,6 @@ import { UsersController } from 'users/users.controller';
     AppController,
     UsersController,
   ],
-  components: [],
+  components: [UsersService],
 })
 export class AppModule {}
