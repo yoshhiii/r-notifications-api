a198901ba376867f6c592c13d1877d1ecd93c800
Chris Walsh <cwalsh@reliaslearning.com>
2018-04-06 11:14:28 -0400

    changing slack hook

--- src/alerts/slack.service.ts
+++ src/alerts/slack.service.ts
@@ -7,16 +7,14 @@ const Slack = require('slack-node');
 @Component()
 export class SlackService {
     slack = new Slack();
-    webhookUri = 'https://hooks.slack.com/services/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';
+    webhookUri = 'https://hooks.slack.com/services/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';
 
     constructor(@Inject('AlertModelToken') private readonly alertModel: Model<Alert>) {
         this.slack.setWebhook(this.webhookUri);
     }
 
     async send(alertDto: AlertDto): Promise<any> {
-        console.log(alertDto);
         return await this.slack.webhook({
-            channel: '#ranchdressing2',
             username: 'relias-notifications',
 text: 'Subject: ' + alertDto.title + `
 Message: ` + alertDto.body,
